1f16201d8325e341484eebe0c4d5a2022a69ad6e
Admin <ilya.steik4@gmail.com>
2019-12-03 18:30:25 +0300

    Updates

---
+++ precache-manifest.e080de0b6312be6eb5d18e5d29773da8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55234bf5c8243115e86bad8a64db7659",
+    "url": "/clickerbattle/index.html"
+  },
+  {
+    "revision": "33191c0f2d955924617c",
+    "url": "/clickerbattle/static/css/2.266626f9.chunk.css"
+  },
+  {
+    "revision": "33191c0f2d955924617c",
+    "url": "/clickerbattle/static/js/2.752ace96.chunk.js"
+  },
+  {
+    "revision": "ac8643fd32fccfb5eb13",
+    "url": "/clickerbattle/static/js/main.36e7ae95.chunk.js"
+  },
+  {
+    "revision": "52d7d27ccb81db53142f",
+    "url": "/clickerbattle/static/js/runtime-main.85f0b673.js"
+  }
+]);
