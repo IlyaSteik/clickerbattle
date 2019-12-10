7534dfa717953e4fd9736a9569400d2000805438
Admin <ilya.steik4@gmail.com>
2019-12-10 16:40:09 +0300

    Updates

--- precache-manifest.7815f9e4fa2ab589a1af4e4a9956f651.js
+++ precache-manifest.7815f9e4fa2ab589a1af4e4a9956f651.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "71a9c117f23e2e073087b50dc9034ed4",
+    "revision": "f5f82a72f6cf8481fbfb9eeb2b3f3abc",
     "url": "/clickerbattle/index.html"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/clickerbattle/static/js/2.095b185d.chunk.js"
   },
   {
-    "revision": "5f286a16eabf836d3c84",
-    "url": "/clickerbattle/static/js/main.aaebda80.chunk.js"
+    "revision": "d6f3fe478b08df9bfe1a",
+    "url": "/clickerbattle/static/js/main.73f09023.chunk.js"
   },
   {
     "revision": "52d7d27ccb81db53142f",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/clickerbattle/precache-manifest.8ae8bd0a62b6bdaff76728759ec85b8c.js"
+  "/clickerbattle/precache-manifest.7815f9e4fa2ab589a1af4e4a9956f651.js"
 );
 
 self.addEventListener('message', (event) => {
