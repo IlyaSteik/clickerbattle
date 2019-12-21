62e462c3a7a7bde81bffa95e197385d432573ba9
Admin <ilya.steik4@gmail.com>
2019-12-21 16:54:19 +0300

    Updates

--- precache-manifest.ea2c5c4cb855625283cabdcf9d639b28.js
+++ precache-manifest.ea2c5c4cb855625283cabdcf9d639b28.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "9d680ccdf6e16febe3568ea16103b6cf",
+    "revision": "2b8913b415c59badf3cf66d0800267a1",
     "url": "/clickerbattle/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/clickerbattle/static/css/2.266626f9.chunk.css"
   },
   {
-    "revision": "463405b828048d4e3e09",
+    "revision": "56c520ccde881e4be487",
     "url": "/clickerbattle/static/css/main.c15b1548.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/clickerbattle/static/js/2.c4acd02e.chunk.js"
   },
   {
-    "revision": "463405b828048d4e3e09",
-    "url": "/clickerbattle/static/js/main.d3eeab7f.chunk.js"
+    "revision": "56c520ccde881e4be487",
+    "url": "/clickerbattle/static/js/main.0105bf92.chunk.js"
   },
   {
     "revision": "52d7d27ccb81db53142f",
