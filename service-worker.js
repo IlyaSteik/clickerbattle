812707d4a028d15b8cfe9fb3a3818ceb63e62346
Admin <ilya.steik4@gmail.com>
2020-01-07 21:39:48 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/clickerbattle/precache-manifest.78fe5a9c6fb2fb3b75e8a851b40d701c.js"
+  "/clickerbattle/precache-manifest.66247178c7d043424cdd1cd79e35fc84.js"
 );
 
 self.addEventListener('message', (event) => {
