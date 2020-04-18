e28ad266cd75f4a5d5d8f2f18bc3b215b84c6039
Admin <ilya.steik4@gmail.com>
2020-04-18 15:54:19 +0300

    Updates

--- precache-manifest.8f668cb53be120b5132d36dd56189768.js
+++ precache-manifest.8f668cb53be120b5132d36dd56189768.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "bee0826b6715ef4cb7759b4ec069c57e",
+    "revision": "ce838f194ab194ab0e7fe08cd5f6a2b5",
     "url": "/clickerbattle/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/clickerbattle/static/css/2.9e627a5a.chunk.css"
   },
   {
-    "revision": "f2f1c539a0ffeac05db4",
+    "revision": "5db56e6d48f2121aa031",
     "url": "/clickerbattle/static/css/main.f2bf327f.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/clickerbattle/static/js/2.a3f48e2e.chunk.js"
   },
   {
-    "revision": "f2f1c539a0ffeac05db4",
-    "url": "/clickerbattle/static/js/main.0d6e66f8.chunk.js"
+    "revision": "5db56e6d48f2121aa031",
+    "url": "/clickerbattle/static/js/main.50d7e23e.chunk.js"
   },
   {
     "revision": "52d7d27ccb81db53142f",
