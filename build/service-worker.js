4164e4b2fa002dd86b0811ea9203a87fa8b75463
IlyaSteik <ilya.steik@mail.ru>
2021-09-21 20:02:42 +0300

    upd minigame

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.3ffcda33eba85a10558492e0a209cc5d.js"
+  "./precache-manifest.a2480273aed93a241541ed86f51173f3.js"
 );
 
 self.addEventListener('message', (event) => {
